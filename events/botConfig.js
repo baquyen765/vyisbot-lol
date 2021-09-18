8f7c21594bc8e395640c130a612b4c8fa3a60321
baquyen765 <85067927+baquyen765@users.noreply.github.com>
2021-09-18 13:38:54 +0700

    Create botConfig.js

---
+++ events/botConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+    token: "ODg3MTM4NTI2MDI0NTk3NTY0.YT_yIg.3_-tE-nGXzfknqafmc7QRTeJfvo",
+    prefix: "v!",
+    "emoji": {
+        "play": "▶️",
+        "stop": "⏹️",
+        "queue": "📄",
+        "success": "☑️",
+        "repeat": "🔁",
+        "error": "❌"
+    },
+    admins: [
+        
+],
+    debug: true,
+    countChannel: "888067552922718288" //Where we counting boys
+};
